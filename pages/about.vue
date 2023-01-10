81abfe83085b96afa0acb6993458ef3efc4820b2
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-01-10 09:47:04 -0500

    feat: create about page
    Created an about page and linked it from the layout navbar.
    Added link to repository to comply with AGPL-3.0-only.

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main>
+    <article>
+      <header>
+        <h2 class="text-red-600 text-2xl">Affero GPL</h2>
+      </header>
+      <p>
+        This project is licensed under the AGPL-3.0-only, the source code for
+        this project is available on
+        <a class="text-red-500" href="https://github.com/lravaglia/traveller"
+          >GitHub</a
+        >.
+      </p>
+    </article>
+  </main>
+</template>
+
+<style scoped></style>
